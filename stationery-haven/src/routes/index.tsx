1a6d7663ff2ae0f1fd4e072491e505e570974058
David <david0kral0@gmail.com>
2026-06-23 17:56:31 +0000

    papírnictví: přidán majitel Jan Seifert do info boxu prodejny

--- stationery-haven/src/routes/index.tsx
+++ stationery-haven/src/routes/index.tsx
@@ -429,6 +429,9 @@ function StoreSection() {
               <InfoRow icon={PhoneIcon} title="+420 123 456 789">
                 info@papirnictvi-zasilkovna.cz
               </InfoRow>
+              <InfoRow icon={UserIcon} title="Jan Seifert">
+                Majitel
+              </InfoRow>
             </div>
           </div>
           <div className="min-h-[320px] lg:min-h-full border-t lg:border-t-0 lg:border-l border-white/5">
@@ -701,7 +704,4 @@ function LeafIcon({ className }: IconProps) {
 function PuzzleIcon({ className }: IconProps) {
   return (
     <svg className={className} viewBox="0 0 24 24" {...stroke}>
-      <path d="M10 3h4v3a2 2 0 1 0 4 0V3h3v4h-3a2 2 0 1 0 0 4h3v4h-4a2 2 0 1 1-4 0v4H6v-4a2 2 0 1 0-3 0H3v-4h3a2 2 0 1 0 0-4H3V3h3v3a2 2 0 1 0 4 0V3Z" />
-    </svg>
-  );
-}
+      <path d="M10 3h4v3a2 2 0 1 0 4 0V3h3v4h-3a2 2 0 1 0 0 4h3v
